5b9c22bce387c55a6e0f21f0e12eb9c91464ccda
Bernhard Bablok <bablokb@gmx.de>
2022-09-15 20:30:51 +0200

    initial commit

---
+++ src/config/adafruit_magtag_2_9_grayscale.py
@@ -0,0 +1,17 @@
+# ----------------------------------------------------------------------------
+# adafruit_magtag_2_9_grayscale.py: board-specific setup for Magtag
+#
+# Author: Bernhard Bablok
+# License: GPL3
+#
+# Website: https://github.com/bablokb/circuitpython-clock
+#
+# ----------------------------------------------------------------------------
+
+import board
+
+class Options:
+  pass
+
+config = Options()
+from wifi_impl_esp32 import WifiImpl as WifiImpl
---
+++ src/wifi_impl_esp32.py
@@ -0,0 +1,61 @@
+# ----------------------------------------------------------------------------
+# wifi_impl_esp32.py: Wifi-implementation for builtin ESP32
+#
+# Author: Bernhard Bablok
+# License: GPL3
+#
+# Website: https://github.com/bablokb/circuitpython-clock
+#
+# ----------------------------------------------------------------------------
+
+import board
+import wifi
+import time
+import socketpool
+import adafruit_requests
+
+class WifiImpl:
+  """ Wifi-implementation for the ESP32 """
+
+  # --- constructor   --------------------------------------------------------
+
+  def __init__(self,config,secrets):
+    """ constructor """
+
+    self._config  = config
+    self._secrets = secrets
+
+  # --- initialze and connect to AP and to remote-port   ---------------------
+
+  def connect(self):
+    """ initialize ESP32 """
+
+    print("connecting to %s" % self._secrets["ssid"])
+    retries = 3
+    while True:
+      try:
+        wifi.radio.connect(self._secrets["ssid"], self._secrets["password"])
+        break
+      except:
+        print("could not connect to %s" % self._secrets["ssid"])
+        retries -= 1
+        if retries == 0:
+          raise
+        time.sleep(1)
+        continue
+    print("connected to %s" % self._secrets["ssid"])
+    pool = socketpool.SocketPool(wifi.radio)
+    self._requests = adafruit_requests.Session(pool)
+
+  # --- execute get-request   -----------------------------------------------
+
+  def get(self,url):
+    """ process get-request """
+
+    return self._requests.get(url)
+
+  # --- no specific deep-sleep mode   ---------------------------------------
+
+  def deep_sleep(self):
+    """ deep_sleep is noop """
+    pass
