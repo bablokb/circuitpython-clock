f22bf563bf753b8d4fd799d45cff09b7c33c0f8c
Bernhard Bablok <bablokb@gmx.de>
2022-09-29 19:42:46 +0200

    implement battery-level

--- src/config/adafruit_magtag_2_9_grayscale.py
+++ src/config/adafruit_magtag_2_9_grayscale.py
@@ -9,9 +9,20 @@
 # ----------------------------------------------------------------------------
 
 import board
+from analogio import AnalogIn
 
 class Options:
   pass
 
 config = Options()
 from wifi_impl_esp32 import WifiImpl as WifiImpl
+
+def hw_setup():
+  """ setup magtag specific hardware """
+  hw_config = Options()
+  hw_config.bat_mon = AnalogIn(board.BATTERY)
+  return hw_config
+
+def bat_level(hw_conf):
+  """ return battery level """
+  return (hw_conf.bat_mon.value / 65535.0) * 3.3 * 2
